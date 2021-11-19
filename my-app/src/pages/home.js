0e47a6fecf066c7d0df5c37cc24a09f43828fa9d
Kitrick Bell <kitrickd.bell@gmail.com>
2021-11-18 23:54:31 -0600

    Added a features component and stlyed features

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -4,16 +4,30 @@ import { FooterContainer } from "../containers/footer";
 import { FaqsContainer } from "../containers/faqs";
 import {HeaderContainer} from "../containers/header"
 import { Header } from "../components/accordian/styles/accordian";
-
+import { OptForm, Feature } from "../components";
 export default function Home(){
 return (
 
     <>
     <HeaderContainer>
+        <Feature>
+            <Feature.Title>Unlimited films, TV programs and more.</Feature.Title>
+            <Feature.SubTitle>Watdh anywhere. Cancel at anytime</Feature.SubTitle>
+        </Feature>
+
+    <OptForm>
+    <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try it Now</OptForm.Button>
+            <OptForm.Text>
+            Ready to watch? Enter your email to create or restart your membership
+            </OptForm.Text>
+
+    </OptForm>
+    </HeaderContainer>
       <JumbotronContainer />
        <FaqsContainer/>
        <FooterContainer/>
-     </HeaderContainer>
+    
     </>
 )
 
