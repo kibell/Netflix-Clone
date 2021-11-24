7647bab6273becc4350c9ab35d41011721a20dca
Kitrick Bell <kitrickd.bell@gmail.com>
2021-11-24 16:28:32 -0600

    hard coding the  titles of the films and series in a new file

--- my-app/src/pages/browse.js
+++ my-app/src/pages/browse.js
@@ -5,8 +5,15 @@ import { useContent } from '../hooks';
 export default function Browse() {
 
     const {series} = useContent('series');
+    const {films} = useContent('films');
     console.log(series)
+    console.log(films)
 
+
+
+
+    //this utility is going to allow us to pass in the series and films
+    
     return <p>Hello form the BROWSE </p>
 
 
