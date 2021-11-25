7337edf12dc2b349af9ce9f724d8dac702f00383
Kitrick Bell <kitrickd.bell@gmail.com>
2021-11-24 18:48:55 -0600

    styled Browser page

--- my-app/src/components/index.js
+++ my-app/src/components/index.js
@@ -1,9 +1,11 @@
-//every component that we create we are going to store it here so that the app.js is cleaner
-export {default as Jumbotron} from './jumbotron';
-export {default as Footer} from './footer'
-export {default as Accordian} from './accordian'
-export {default as OptForm} from './opt-form'
-export {default as Header} from './header'
-export {default as Feature} from './features'
-export {default as Form} from './form'
-export {default as Profiles} from './profiles'
+export { default as Accordian } from './accordian';
+export { default as Card } from './card';
+export { default as Jumbotron } from './jumbotron';
+export { default as Feature } from './features';
+export { default as OptForm } from './opt-form';
+export { default as Form } from './form';
+export { default as Profiles } from './profiles';
+export { default as Footer } from './footer';
+export { default as Header } from './header';
+export { default as Loading } from './loading';
+export { default as Player } from './player';
