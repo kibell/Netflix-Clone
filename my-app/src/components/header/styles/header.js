8cdafb637171afbe202b6a6458c13ace5f552d1f
Kitrick Bell <kitrickd.bell@gmail.com>
2021-11-24 23:09:31 -0600

    testing app and it works as exoected

--- my-app/src/components/header/styles/header.js
+++ my-app/src/components/header/styles/header.js
@@ -7,7 +7,7 @@ export const Background = styled.div`
   background: linear-gradient(to bottom, rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.35)), url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg')}) top left / cover
     no-repeat;
 
-  @media (max-width: 1100px) {
+  @media (max-width: 900px) {
     ${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `background: none;`}
   }
 `;
